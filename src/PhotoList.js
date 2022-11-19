2867f89b0fb5d9b7b2a4362ba1e882078a5d9aa8
WooDaeHyun <po668312@naver.com>
2022-11-20 02:24:53 +0900

    feat : basic components for infinite scroll

---
+++ src/PhotoList.js
@@ -0,0 +1,49 @@
+/*initialState: [
+  {
+    id: 1,
+    imageUrl: '',
+  }
+]
+*/
+
+export default function PhotoList({ $target, initialState, onScrollEnded }) {
+  let isInitialize = false;
+  const $photoList = document.createElement("div");
+  $target.appendChild($photoList);
+  this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  this.render = () => {
+    if (!isInitialize) {
+      $photoList.innerHTML = `
+      <ul class="PhotoList__photos"></ul>
+      <button class="PhotoList__loadMore" style="width:100%; height: 200px; font-size: 20px;">Load More</button>
+      `;
+      isInitialize = true;
+    }
+    const { photos } = this.state;
+    const $photos = $photoList.querySelector(`.PhotoList__photos`);
+
+    photos.forEach((photo) => {
+      if ($photos.querySelector(`li[data-id="${photo.id}"]`) === null) {
+        const $li = document.createElement("li");
+        $li.setAttribute("data-id", photo.id);
+        $li.style = "list-style: none;";
+        $li.innerHTML = `<img width="100%" src="${photo.imagePath}" />`;
+
+        $photos.appendChild($li);
+      }
+    });
+  };
+  this.render();
+
+  $photoList.addEventListener("click", (e) => {
+    if (e.target.className === "PhotoList__loadMore" && !this.state.isLoading) {
+      onScrollEnded();
+    }
+  });
+}
